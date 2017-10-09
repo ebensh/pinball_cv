557ebfe2b500fead33397d4a6c436ab8d48c2583
ebensh <ebensh@gmail.com>
2017-10-09 02:08:56 -0400

    Modified main.py to apply the heat mask

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python
 
+import argparse
 import cv2
 import numpy as np
 
-def main():
-  cap = cv2.VideoCapture('short_clip.mp4')
-  base_frame = None
+parser = argparse.ArgumentParser(description='Track a pinball.')
+parser.add_argument('--infile', required=True, type=str, help='Input video file path.')
+parser.add_argument('--heatmap', required=True, type=str, help='Heat map image file path.')
+parser.add_argument('--debug', default=False, type=bool, help='Display processing steps.')
+args = parser.parse_args()
 
-  cv2.namedWindow('base_frame', cv2.WINDOW_NORMAL)
-  cv2.namedWindow('raw_frame', cv2.WINDOW_NORMAL)
-  cv2.namedWindow('p1', cv2.WINDOW_NORMAL)
-  cv2.namedWindow('p2', cv2.WINDOW_NORMAL)
+def main():
+  cap = cv2.VideoCapture('intro_removed.mp4')
+  base_frame = None
+  heat_map = cv2.imread('heat_map_intro_removed.png', cv2.COLOR_BGR2GRAY)
+
+  if args.debug:
+    cv2.namedWindow('base_frame', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('raw_frame', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('heat_map', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('p1', cv2.WINDOW_NORMAL)
+
+  # Note that we invert the heat_map here, so only those *cold* portions of the
+  # heat map will be kept when we use it as a mask later.
+  _, heat_map = cv2.threshold(heat_map, 2, 255, cv2.THRESH_BINARY_INV)
+  # Dilate then erode, to "join" the pixels into blobs
+  heat_map = cv2.morphologyEx(heat_map, cv2.MORPH_CLOSE, kernel=np.ones((5,5),np.uint8))
+  #heat_map = heat_map > 0  # Convert to logical map
+  if args.debug: cv2.imshow('heat_map', heat_map)
   
   while cap.isOpened():
-    ret, raw_frame = cap.read()
-    cv2.imshow('raw_frame', raw_frame)
-    
+    grabbed, raw_frame = cap.read()
+    if not grabbed: break
+    if args.debug: cv2.imshow('raw_frame', raw_frame)
+
     if base_frame is None:
-      base_frame = raw_frame
-      prev_frame = raw_frame
-      cv2.imshow('base_frame', base_frame)
-    
+      base_frame = cv2.bitwise_and(raw_frame, cv2.cvtColor(heat_map, cv2.COLOR_GRAY2BGR))
+      if args.debug: cv2.imshow('base_frame', base_frame)
+
     frame = cv2.absdiff(raw_frame, base_frame)
-    cv2.imshow('p1', frame)
-
-    _, frame = cv2.threshold(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 100, 255, cv2.THRESH_BINARY)
-    cv2.imshow('p2', frame)
-
-    prev_frame = frame
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+    frame = cv2.bitwise_and(frame, heat_map)
     
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+    cv2.imshow('p1', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
   cap.release()
