af5477b5c4cc45e7ed52ab0e8aaa82d26e18021f
ebensh <ebensh@gmail.com>
2017-10-09 11:46:38 -0400

    Heat mask processed, main function running, still too noisy.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import argparse
-import common
 import cv2
 import numpy as np
+
+import common
 
 def main():
   cap = cv2.VideoCapture(args.infile)
@@ -12,8 +13,7 @@ def main():
 
   # Note that we invert the heat_map here, so only those *cold* portions of the
   # heat map will be kept when we use it as a mask later.
-  _, heat_map = cv2.threshold(heat_map, 2, 255, cv2.THRESH_BINARY_INV)
-  #heat_map = heat_map > 0  # Convert to logical map
+  heat_map = 255 - heat_map
   
   while cap.isOpened():
     grabbed, raw_frame = cap.read()
@@ -21,14 +21,14 @@ def main():
     common.display_image(raw_frame, 'raw_frame', True)
 
     if base_frame is None:
-      base_frame = cv2.bitwise_and(raw_frame, cv2.cvtColor(heat_map, cv2.COLOR_GRAY2BGR))
+      base_frame = raw_frame
       common.display_image(base_frame, 'base_frame', args.display_all_images)
       common.display_image(heat_map, 'heat_map', args.display_all_images)
 
     frame = cv2.absdiff(raw_frame, base_frame)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
     frame = cv2.bitwise_and(frame, heat_map)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, frame = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
 
     common.display_image(frame, show=args.display_all_images)
     
--- view_heat_mask.py
+++ view_heat_mask.py
@@ -1,45 +1,27 @@
 #!/usr/bin/python
 
 import argparse
-import inspect
 import cv2
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Create a heat mask for a given video.')
-parser.add_argument('--infile', required=True, type=str, help='Input image path.')
-parser.add_argument('--outfile', type=str, help='Output image path.')
-parser.add_argument('--debug', default=True, type=bool, help='Display processing steps.')
-args = parser.parse_args()
-
-debug_step = 0
-def display_image(img, title=None, show=args.debug):
-  global debug_step
-  if args.debug:
-    if title is None:
-      # Get the caller's line number so we can identify which point in the
-      # process we're at without uniquely naming each one.
-      frame, filename, line_num, function_name, lines, index = inspect.stack()[1]
-      title = "Step {0}, Line {1}".format(debug_step, line_num)
-      debug_step += 1
-    cv2.imshow(title, img)
-
+import common
 
 def main():
   img = cv2.imread(args.infile)
-  display_image(img, 'original')
+  common.display_image(img, 'original', args.display_all_images)
 
   # Apply effects here to see outputs.
   _, img = cv2.threshold(img, 3, 255, cv2.THRESH_BINARY)
-  display_image(img)
+  common.display_image(img, show=args.display_all_images)
   
-  kernel = np.ones((5, 5), np.uint8)
+  kernel = np.ones((9, 9), np.uint8)
   img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-  display_image(img)
+  common.display_image(img, show=args.display_all_images)
 
   if args.outfile:
     cv2.imwrite(args.outfile, img)
 
-  if args.debug:
+  if args.display_all_images:
     while True:
       if cv2.waitKey(0) & 0xFF == ord('q'):
         break
@@ -48,4 +30,10 @@ def main():
 
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Create a heat mask for a given video.')
+  parser.add_argument('--infile', required=True, type=str, help='Input image path.')
+  parser.add_argument('--outfile', type=str, help='Output image path.')
+  parser.add_argument('--display_all_images', default=False, type=bool,
+                      help='Display all (debug) images.')
+  args = parser.parse_args()
   main()
