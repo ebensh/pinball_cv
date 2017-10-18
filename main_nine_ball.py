094fe69007b734ce0a4a4547dbdbbf46dc2db7b5
ebensh <ebensh@gmail.com>
2017-10-18 01:10:54 -0600

    Pinball tracking with circles working :)

--- main_nine_ball.py
+++ main_nine_ball.py
@@ -67,24 +67,24 @@ def main():
   CURRENT_FRAME_INDEX = FRAME_BUFFER_SIZE/2
 
   cap = cv2.VideoCapture(args.infile)
-  detector = get_blob_detector()
+
+  detector = get_blob_detector()  # REMEMBER: DETECTS BLACK OBJECTS, WHITE BG.
 
   _, raw_frame = cap.read()
   frame_buffer = common.FrameBuffer(FRAME_BUFFER_SIZE, raw_frame.shape)
 
+  video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'),
+                          30.0, raw_frame.shape[1::-1])
+
   pinball_field_poly = get_pinball_field_poly(raw_frame)
   pinball_field_mask = get_pinball_field_mask(raw_frame, pinball_field_poly)
   pinball_perspective_transform = get_perspective_transform(raw_frame, pinball_field_poly)
-  
-  # Loop until the frame buffer is full at the start.
-  for i in xrange(FRAME_BUFFER_SIZE):
-    if not cap.isOpened(): break
-    grabbed, raw_frame = cap.read()
-    if not grabbed: break
-    frame_buffer.append(raw_frame)
 
-  cv2.namedWindow('past_stats', cv2.WINDOW_NORMAL)
-  cv2.namedWindow('combined', cv2.WINDOW_NORMAL)
+  pinball_points = np.zeros(raw_frame.shape, dtype=np.uint8)
+
+  if args.display_all_images:
+    cv2.namedWindow('past_stats', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('combined', cv2.WINDOW_NORMAL)
   frame_count = 0
   while cap.isOpened():
     grabbed, raw_frame = cap.read()
@@ -97,7 +97,7 @@ def main():
     pinball_area = cv2.bitwise_and(raw_frame, raw_frame, mask=pinball_field_mask)
     pinball_area = cv2.warpPerspective(pinball_area, pinball_perspective_transform,
                                        dsize=raw_frame.shape[1::-1])
-    common.display_image(pinball_area, 'pinball_area')
+    common.display_image(pinball_area, 'pinball_area', args.display_all_images)
     frame_buffer.append(pinball_area)
 
     past = frame_buffer.get_view(None, CURRENT_FRAME_INDEX)
@@ -155,84 +155,31 @@ def main():
       255 * foreground_mask.astype(np.uint8),
       255 * changing_mask.astype(np.uint8),
       final_mask,
-      final_mask_polished]))
+      final_mask_polished]), 'masks', args.display_all_images)
 
+    keypoints = detector.detect(cv2.bitwise_not(final_mask_polished))
+    print "Keypoints:", keypoints
+    # Fade away old points.
+    pinball_points = trim_to_uint8(pinball_points.astype(np.int16) - 1)
+    # Then draw new points.
+    pinball_points = cv2.drawKeypoints(pinball_points, keypoints, np.array([]), (0,255,255),
+                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    pinball_points_display = current_frame.copy()
+    pinball_points_display[pinball_points > 0] = pinball_points[pinball_points > 0]
+    common.display_image(pinball_points_display, 'pinball_points')
+    if video:
+      video.write(pinball_points_display)
         
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
     continue
-      
-      
-      
-    
-    mean_past_gray = cv2.cvtColor(cv2.convertScaleAbs(mean_past), cv2.COLOR_BGR2GRAY)
-    mean_future_gray = cv2.cvtColor(cv2.convertScaleAbs(mean_future), cv2.COLOR_BGR2GRAY)
-
-    count_past = np.zeros(current_frame_gray.shape, dtype=np.uint8)
-    count_future = np.zeros(current_frame_gray.shape, dtype=np.uint8)
-    for past_frame, past_frame_gray in frames_past:
-      count_past += (cv2.absdiff(past_frame_gray, mean_past_gray) > 10)
-    for future_frame, future_frame_gray in frames_future:
-      count_future += (cv2.absdiff(future_frame_gray, mean_future_gray) > 10)
-
-    print count_past
-    _, mask_past = cv2.threshold(count_past, 3, 255, cv2.THRESH_BINARY_INV)
-    _, mask_future = cv2.threshold(count_future, 3, 255, cv2.THRESH_BINARY_INV)
-    common.display_image(mask_past, 'mask_past')
-
-    # accumulated_past_gray = (accumulated_past * 255.0 / accumulated_past.max()).astype(np.uint8)
-    # accumulated_past_gray = cv2.cvtColor(accumulated_past_gray, cv2.COLOR_BGR2GRAY)
-    # past_difference = cv2.absdiff(current_frame_gray, accumulated_past_gray)
-    # past_difference = cv2.GaussianBlur(past_difference, (5, 5), 0)
-    # _, past_difference = cv2.threshold(past_difference, 50, 255, 0)
-    # accumulated_future_gray = (accumulated_future * 255.0 / accumulated_future.max()).astype(np.uint8)
-    # accumulated_future_gray = cv2.cvtColor(accumulated_future_gray, cv2.COLOR_BGR2GRAY)
-    # future_difference = cv2.absdiff(current_frame_gray, accumulated_future_gray)
-    # future_difference = cv2.GaussianBlur(future_difference, (5, 5), 0)
-    # #_, future_difference = cv2.threshold(future_difference, 50, 255, 0)
-    
-    #_, mask_past = cv2.threshold(delta_past, 25, 255, cv2.THRESH_BINARY_INV)
-    #_, mask_future = cv2.threshold(delta_future, 25, 255, cv2.THRESH_BINARY_INV)
-    
-    final = cv2.absdiff(current_frame, cv2.convertScaleAbs(accumulated_past))
-    final = cv2.bitwise_and(final, final, mask=mask_past)
-    final = cv2.bitwise_and(final, final, mask=mask_future)
-    final = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
-    final = cv2.GaussianBlur(final, (3, 3), 0)
-    _, final = cv2.threshold(final, 40, 255, cv2.THRESH_BINARY)
-
-    keypoints = detector.detect(cv2.bitwise_not(final))
-    print keypoints
-    current_frame_with_keypoints = cv2.drawKeypoints(current_frame, keypoints, np.array([]), (255,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    common.display_image(final, 'final')
-    common.display_image(current_frame_with_keypoints, 'current_frame_with_keypoints')    
-    
-    #past_difference = (accumulated_past * 255.0 / accumulated_past.max()).astype(np.uint8)
-    #future_difference = (accumulated_future * 255.0 / accumulated_future.max()).astype(np.uint8)
-    #past_difference = cv2.absdiff(current_frame_gray, cv2.cvtColor(past_difference, cv2.COLOR_BGR2GRAY))
-    #future_difference = cv2.absdiff(current_frame_gray, cv2.cvtColor(future_difference, cv2.COLOR_BGR2GRAY))
-    frame_printer.add_image(accumulated_past, 'accumulated_past')
-    #frame_printer.add_image(accumulated_past_gray, 'accumulated_past_gray')
-    #frame_printer.add_image(past_difference, 'past_difference')
-    frame_printer.add_image(delta_past, 'delta_past')
-    frame_printer.add_image(current_frame, 'current_frame')
-    frame_printer.add_image(delta_future, 'delta_future')
-    #frame_printer.add_image(future_difference, 'future_difference')
-    #frame_printer.add_image(accumulated_future_gray, 'accumulated_future_gray')
-    frame_printer.add_image(accumulated_future, 'accumulated_future')
-    
-    
-
-    frames_past.append((current_frame, current_frame_gray))
-
-    combined_image = frame_printer.get_combined_image()
-    common.display_image(combined_image, 'combined')
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
   print 'Frames processed: %d' % frame_count
 
   cap.release()
+  video.release()
   cv2.destroyAllWindows()
 
 if __name__ == '__main__':
