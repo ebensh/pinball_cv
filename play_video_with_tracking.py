e2e99b13026e4544e7c32fd9a0566c4480f7ae30
ebensh <ebensh@gmail.com>
2017-10-31 17:20:00 -0400

    Added some attempts at lerping and extrapolating, but I think the distance is off.

--- play_video_with_tracking.py
+++ play_video_with_tracking.py
@@ -7,16 +7,18 @@ import cv2
 from itertools import islice, izip
 import numpy as np
 import pickle
+import sys
 
 import common
 
-COLOR_NEGATIVE = (0, 0, 255)    # Red
-COLOR_UNKNOWN  = (255, 0, 255)  # Purple
-COLOR_POSITIVE = (0, 255, 0)    # Green
+COLOR_NEGATIVE     = (0, 0, 255)    # Red
+COLOR_UNKNOWN      = (255, 0, 255)  # Purple
+COLOR_POSITIVE     = (0, 255, 0)    # Green
+COLOR_EXTRAPOLATED = (0, 255, 255)  # Yellow
 
-LABEL_NEGATIVE = -1
-LABEL_UNKNOWN  = 0
-LABEL_POSITIVE = 1
+LABEL_NEGATIVE = 0
+LABEL_UNKNOWN  = 1
+LABEL_POSITIVE = 2
 class LabeledPoint(object):
   def __init__(self, x, y, size, label=LABEL_UNKNOWN):
     self.x = x
@@ -27,6 +29,8 @@ class LabeledPoint(object):
     if self.label == LABEL_NEGATIVE: return COLOR_NEGATIVE
     elif self.label == LABEL_UNKNOWN: return COLOR_UNKNOWN
     return COLOR_POSITIVE
+  def xy(self):
+    return (self.x, self.y)
 
 
 class LabeledFrame(object):
@@ -34,16 +38,30 @@ class LabeledFrame(object):
     self.frame_index = frame_index
     self.labeled_points = labeled_points
 
+  def add_point(self, point):
+    self.labeled_points.append(point)
+
+  def num_points(self, label=None):
+    if label is None:
+      return len(self.labeled_points)
+    return sum(map(lambda lp: lp.label == label, self.labeled_points))
+
+  def positive_point(self):
+    for point in self.labeled_points:
+      if point.label == LABEL_POSITIVE:
+        return point
+    return None
+
   def draw_on_mask(self, mask):
     for lp in self.labeled_points:
       # Filled circle in the mask - use a small size to avoid masking out the ball.
-      cv2.circle(mask, (lp.x, lp.y), 3, 255, -1)
+      cv2.circle(mask, (lp.x, lp.y), 4, 255, -1)
     return mask
 
   def draw_on_frame(self, frame):
     # TODO: Handle more than one pinball :)
     for lp in self.labeled_points:
-      cv2.circle(frame, (lp.x, lp.y), 3, lp.color(), -1)
+      cv2.circle(frame, (lp.x, lp.y), 4, lp.color(), -1)
       cv2.putText(frame, str(self.frame_index), (lp.x, max(lp.y-10, 0)),
                   cv2.FONT_HERSHEY_PLAIN, 1, lp.color(), 2)
     return frame
@@ -77,9 +95,9 @@ def main():
   # points.
   keypoints_mask_history = np.zeros(background.shape[:2], np.uint8)
 
-  previous_pinball_points = deque(maxlen=10)
-
   frame_index = 0
+  pass_iteration = 0
+  point_label_counts = {pass_iteration: [0, 0, 0]}  # Negative, Unknown, Positive
   while cap.isOpened():
     grabbed, raw_frame = cap.read()
     if not grabbed: break
@@ -87,6 +105,8 @@ def main():
 
     labeled_frame = labeled_frames[frame_index]
     labeled_points = labeled_frame.labeled_points
+    def prev_frame(n): return labeled_frames[max(frame_index - n, 0)]
+    def next_frame(n): return labeled_frames[min(frame_index + n, len(labeled_frames) - 1)]
 
     # Create a mask of the current keypoints.
     keypoints_mask = np.zeros(frame.shape[:2], np.uint8)
@@ -94,32 +114,91 @@ def main():
 
     # Create a historical mask of the previous N frames.
     historical_mask = np.zeros(frame.shape[:2], np.uint8)
-    for past_frame in labeled_frames[frame_index-FADE_STEP_COUNT:frame_index-1]:
+    for past_frame in labeled_frames[max(frame_index-FADE_STEP_COUNT,0):frame_index-1]:
       past_frame.draw_on_mask(historical_mask)
 
     # If a current point is in the historical mask then we filter it.
     for lp in labeled_points:
-      if historical_mask[lp.y, lp.x] > 0:
+      if lp.label == LABEL_UNKNOWN and historical_mask[lp.y, lp.x] > 0:
         lp.label = LABEL_NEGATIVE
 
-    if sum(map(lambda lp: lp.label == LABEL_UNKNOWN, labeled_points)) == 1:
+    if labeled_frame.num_points(LABEL_UNKNOWN) == 1:
+      # If only one point remains UNKNOWN, then we flip it to positive (though we
+      # may later undo this!)
       for lp in labeled_points:
         if lp.label == LABEL_UNKNOWN:
           lp.label = LABEL_POSITIVE
           break
-    # TODO: If only one point remains, flip it to positive?
+
+    def extrapolate(lp1, lp2):
+      vx = lp2.x - lp1.x
+      vy = lp2.y - lp1.y
+      return (lp2.x + vx, lp2.y + vy, int((lp1.size + lp2.size) / 2))
+    def lerp((x1, y1), (x2, y2)):
+      return ((x1 + x2) / 2,
+              (y1 + y2) / 2)
+    def dist((x1, y1), (x2, y2)):
+      return (x2 - x1)**2 + (y2 - y1)**2
     
-    # TODO: Create an arrowed line connecting previous positive points?
-    #for p1, p2 in izip(previous_pinball_points, islice(previous_pinball_points, 1, None)):
-    #  cv2.arrowedLine(frame, p1[:2], p2[:2], (0, 255, 0))
+    # Extrapolate from the n-2 and n-1 frame to get a past prediction of current
+    # location as a vector of the same magnitude and direction (best guess).
+    two_before = prev_frame(2).positive_point()
+    one_before = prev_frame(1).positive_point()
+    past_extrapolation = None
+    if two_before and one_before:
+      past_extrapolation = extrapolate(two_before, one_before)
+      cv2.arrowedLine(frame, two_before.xy(), past_extrapolation[:2],
+                      COLOR_EXTRAPOLATED)
+
+    one_after = next_frame(1).positive_point()
+    two_after = next_frame(2).positive_point()
+    future_extrapolation = None
+    if two_after and one_after:
+      future_extrapolation = extrapolate(two_after, one_after)
+      cv2.arrowedLine(frame, two_after.xy(), future_extrapolation[:2],
+                      COLOR_EXTRAPOLATED)
+
+    if one_before and one_after:
+      before_and_after_lerp = lerp(one_before.xy(), one_after.xy())
+      cv2.arrowedLine(frame, one_before.xy(), before_and_after_lerp,
+                      COLOR_EXTRAPOLATED)
+      cv2.arrowedLine(frame, one_after.xy(), before_and_after_lerp,
+                      COLOR_EXTRAPOLATED)
+
+    if past_extrapolation and future_extrapolation:
+      # This is where we expect the ball to be, based on past and future
+      # POSITIVE points.
+      past_and_future_lerp = lerp(past_extrapolation[:2], future_extrapolation[:2])
+      least_distance, closest_point = sys.maxint, None
+      for lp in labeled_points:
+        distance = dist(lp.xy(), past_and_future_lerp)
+        if distance < least_distance:
+          least_distance = distance
+          closest_point = lp
+      if closest_point and least_distance < 100:
+        closest_point.label = LABEL_POSITIVE
+      for lp in labeled_points:
+        if lp.label != LABEL_POSITIVE:
+          lp.label = LABEL_NEGATIVE
+            
+    for lp in labeled_points:
+      point_label_counts[pass_iteration][lp.label] += 1
 
     common.display_image(raw_frame, show=True)
     for lf in labeled_frames[frame_index-10:frame_index+1]:
       lf.draw_on_frame(frame)
+    for iteration, label_counts in point_label_counts.iteritems():
+      cv2.putText(frame, str(label_counts), (0, 12 * (iteration + 1)),
+                  cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
     common.display_image(frame, show=True)
-    video.write(frame)    
-
+    video.write(frame)
+    
     frame_index += 1
+    if frame_index == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+      cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+      frame_index = 0
+      pass_iteration += 1
+      point_label_counts[pass_iteration] = [0, 0, 0]
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
     
