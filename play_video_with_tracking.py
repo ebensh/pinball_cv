5924609da333be8a7f2e3468ca80dfec19e0ec2e
ebensh <ebensh@gmail.com>
2017-10-31 01:24:15 -0400

    Added frame number labeling, structs to hold point labels and frame labels.

--- play_video_with_tracking.py
+++ play_video_with_tracking.py
@@ -10,8 +10,46 @@ import pickle
 
 import common
 
+COLOR_NEGATIVE = (0, 0, 255)    # Red
+COLOR_UNKNOWN  = (255, 0, 255)  # Purple
+COLOR_POSITIVE = (0, 255, 0)    # Green
+
+LABEL_NEGATIVE = -1
+LABEL_UNKNOWN  = 0
+LABEL_POSITIVE = 1
+class LabeledPoint(object):
+  def __init__(self, x, y, size, label=LABEL_UNKNOWN):
+    self.x = x
+    self.y = y
+    self.size = size
+    self.label = label
+  def color(self):
+    if self.label == LABEL_NEGATIVE: return COLOR_NEGATIVE
+    elif self.label == LABEL_UNKNOWN: return COLOR_UNKNOWN
+    return COLOR_POSITIVE
+
+
+class LabeledFrame(object):
+  def __init__(self, frame_index, labeled_points):
+    self.frame_index = frame_index
+    self.labeled_points = labeled_points
+
+  def draw_on_mask(self, mask):
+    for lp in self.labeled_points:
+      # Filled circle in the mask - use a small size to avoid masking out the ball.
+      cv2.circle(mask, (lp.x, lp.y), 3, 255, -1)
+    return mask
+
+  def draw_on_frame(self, frame):
+    # TODO: Handle more than one pinball :)
+    for lp in self.labeled_points:
+      cv2.circle(frame, (lp.x, lp.y), 3, lp.color(), -1)
+      cv2.putText(frame, str(self.frame_index), (lp.x, max(lp.y-10, 0)),
+                  cv2.FONT_HERSHEY_PLAIN, 1, lp.color(), 2)
+    return frame
+
 def main():
-  FADE_STEP_COUNT = 30
+  FADE_STEP_COUNT = 60  # ~2 seconds
   game_config = ConfigParser.ConfigParser()
   game_config.read(args.game_config)
   input_rows = game_config.getint('PinballFieldVideo', 'rows')
@@ -23,6 +61,14 @@ def main():
   frame_to_keypoints = None
   with open(game_config.get('PinballFieldVideo', 'keypoints_path'), 'rb') as keypoints_file:
     frame_to_keypoints = pickle.load(keypoints_file)
+  num_frames = len(frame_to_keypoints)
+  assert num_frames == max(frame_to_keypoints.keys()) + 1
+
+  labeled_frames = [None] * num_frames
+  for index in xrange(num_frames):
+    keypoints = [LabeledPoint(int(round(x)), int(round(y)), int(round(size)))
+                 for x, y, size in frame_to_keypoints[index]]
+    labeled_frames[index] = LabeledFrame(index, keypoints)
 
   background = cv2.imread(args.background, cv2.IMREAD_COLOR)
   # We use point_history as a mask that fades over time. Each time we see a
@@ -37,66 +83,44 @@ def main():
   while cap.isOpened():
     grabbed, raw_frame = cap.read()
     if not grabbed: break
-    frame = raw_frame.copy()
+    frame = raw_frame.copy()  # Unnecessary copy to preserve original. Remove?
 
-    keypoints = frame_to_keypoints.get(frame_index + 4, [])
-    keypoints = [(int(round(x)), int(round(y)), int(round(size))) for x, y, size in keypoints]
-    print frame_index, keypoints
+    labeled_frame = labeled_frames[frame_index]
+    labeled_points = labeled_frame.labeled_points
 
-    # Create a mask of the current keypoints, using small size for locality.
+    # Create a mask of the current keypoints.
     keypoints_mask = np.zeros(frame.shape[:2], np.uint8)
-    for x, y, size in keypoints:
-      # Filled circle in the mask - use a small size to avoid masking out the ball.
-      cv2.circle(keypoints_mask, (x, y), 6, 255, -1)
+    labeled_frame.draw_on_mask(keypoints_mask)
 
-    # If a point is in the current mask but *NOT* the historical mask, then we
-    # keep it.
-    keypoints_kept = set()
-    keypoints_filtered = set()
-    for x, y, size in keypoints:
-      if np.logical_and(keypoints_mask[y, x] > 0, keypoints_mask_history[y, x] == 0):
-        keypoints_kept.add((x, y, size))
-      else:
-        keypoints_filtered.add((x, y, size))
+    # Create a historical mask of the previous N frames.
+    historical_mask = np.zeros(frame.shape[:2], np.uint8)
+    for past_frame in labeled_frames[frame_index-FADE_STEP_COUNT:frame_index-1]:
+      past_frame.draw_on_mask(historical_mask)
 
-    # def euclidean_distance(x1, y1, x2, y2): return (x2 - x1)**2 + (y2 - y1)**2
-    # pinball_x = pinball_y = None
-    # if previous_pinball_points:
-    #   pinball_x, pinball_y, _ = previous_pinball_points[-1]
-    #   for x, y, size in list(keypoints_kept):  # Copy the list so we can remove while iterating
-    #     if euclidean_distance(x, y, pinball_x, pinball_y) >= 100:
-    #       keypoints_kept.remove((x, y, size))
-    #       keypoints_filtered.add((x, y, size))
+    # If a current point is in the historical mask then we filter it.
+    for lp in labeled_points:
+      if historical_mask[lp.y, lp.x] > 0:
+        lp.label = LABEL_NEGATIVE
 
-    kept_color = (255, 0, 255)  # Purple if we're not sure.
-    if len(keypoints_kept) == 1:
-      kept_color = (0, 255, 0)  # Green if we're sure.
-      previous_pinball_points.append(list(keypoints_kept)[0])
-    filtered_color = (0, 0, 255)  # Red if we're filtering.
+    if sum(map(lambda lp: lp.label == LABEL_UNKNOWN, labeled_points)) == 1:
+      for lp in labeled_points:
+        if lp.label == LABEL_UNKNOWN:
+          lp.label = LABEL_POSITIVE
+          break
+    # TODO: If only one point remains, flip it to positive?
+    
+    # TODO: Create an arrowed line connecting previous positive points?
+    #for p1, p2 in izip(previous_pinball_points, islice(previous_pinball_points, 1, None)):
+    #  cv2.arrowedLine(frame, p1[:2], p2[:2], (0, 255, 0))
 
-    for x, y, size in keypoints_kept:
-      cv2.circle(frame, (x, y), int(size/2), kept_color, -1)
-    for x, y, size in keypoints_filtered:
-      cv2.circle(frame, (x, y), int(size/2), filtered_color, -1)
-
-      #cv2.drawMarker(frame, (int(x), int(y)), color, markerSize=int(size/2),
-        #               line_type=cv2.MARKER_TILTED_CROSS)
-
-    for p1, p2 in izip(previous_pinball_points, islice(previous_pinball_points, 1, None)):
-      cv2.arrowedLine(frame, p1[:2], p2[:2], (0, 255, 0))
-
-    # Add our current mask to the history.
-    keypoints_mask_history = cv2.subtract(keypoints_mask_history, 255 / FADE_STEP_COUNT)
-    keypoints_mask_history = np.maximum(keypoints_mask_history, keypoints_mask)
-
+    common.display_image(raw_frame, show=True)
+    for lf in labeled_frames[frame_index-10:frame_index+1]:
+      lf.draw_on_frame(frame)
     common.display_image(frame, show=True)
-    common.display_image(keypoints_mask, show=True)
-    common.display_image(keypoints_mask_history, show=True)
-    video.write(frame)
-    
-    
+    video.write(frame)    
+
     frame_index += 1
-    if cv2.waitKey(33) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
     
   cap.release()
